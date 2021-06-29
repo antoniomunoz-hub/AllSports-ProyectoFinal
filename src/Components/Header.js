335dc598e236e0b2a7a3595a9fcd5063be6fe85a
antoniomunoz-hub <antonio.bjjrga@gmail.com>
2021-06-29 13:59:16 +0200

    logos con clicks de redireccion y hovers

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import "../StylesPages/Header.css";
 import {useHistory} from "react-router-dom";
-
+import Tooltip from "react-simple-tooltip"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 
  
 
@@ -16,24 +16,42 @@ export default function Header() {
     const goPerfil=()=>{
         history.push("/perfil");
     }
+    const goEdit=()=>{
+        history.push("/editperfil");
+    }
+    const singOut=()=>{
+        history.push("/");
+    }
 
     return (
         <nav className="nav">
+            
             <div className="logo" onClick={()=>goHome()}>
+                <Tooltip content="Home">
                 <img src="../logo/logo.svg" alt=""/>
+                </Tooltip>
             </div>  
 
             <div className="search">
                 <input type="search" name="search" id="serch" placeholder=" &#x1F50D; Type here to search "/>
-
             </div>
+            
             <div className="actions">
-            <div className="alert">
-            <FontAwesomeIcon className="iconbell" icon={faBell}/>
-            </div>
+            
             <div className="avatar" onClick={()=>goPerfil()}>
                 <img src="https://randomuser.me/api/portraits/men/22.jpg" alt=""/>
             </div>
+
+            <div className="alert" onClick={()=>goEdit()}>
+                <FontAwesomeIcon className="iconbell" icon={faEdit}/>
+                
+            </div>
+            <div className="alert" onClick={()=>singOut()}>
+                <FontAwesomeIcon className="iconbell" icon={faSignOutAlt}/>
+            </div>
+            
+            
+        
             </div>
 
         </nav>
